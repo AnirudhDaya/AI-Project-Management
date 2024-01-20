748f95b4756a107ea3b202bcc4adf5579ec69009
Anirudh Dayanand <anirudhdaya@gmail.com>
2024-01-21 02:20:53 +0530

    Minor bug fixes

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-
+  
   router.push("/login"); // Redirect to login page on component mount
 
   // return (
